9793a5ded28800f5f49348436f5e0006e17cee13
Aditya Nalge <adinalge@gmail.com>
2020-11-06 19:00:55 -0800

    Adding CORS

--- backend-code/index.js
+++ backend-code/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const cors = require('cors');
 const app = express()
 const port = 3000
 
-app.get('/healthz', (req, res) => {
+app.get('/healthz', cors(), (req, res) => {
 
     res.send('Tilt-o-Workout App Is Up And Healthy - Timestamp : ' + new Date().toUTCString());
 
