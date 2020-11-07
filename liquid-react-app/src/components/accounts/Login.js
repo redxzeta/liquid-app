24c597d18fd84b2a867917ab5f5e34b822d8bb84
jordanhilado <37820188+jordanhilado@users.noreply.github.com>
2020-11-06 20:34:36 -0800

    Login and sign up forms created

--- liquid-react-app/src/components/accounts/Login.js
+++ liquid-react-app/src/components/accounts/Login.js
@@ -1,5 +1,24 @@
 import React from "react";
+import './login.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Login() {
-  return <div>Login</div>;
+  return <div>
+    <h1>Login</h1>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="Username" placeholder="Username" />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Login
+      </Button>
+    </Form>
+  </div>;
 }
--- liquid-react-app/src/components/accounts/SignUp.js
+++ liquid-react-app/src/components/accounts/SignUp.js
@@ -1,5 +1,28 @@
 import React from "react";
+import './login.css';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-export default function SignUp() {
-  return <div>Signup</div>;
+export default function Login() {
+  return <div>
+    <h1>Sign Up</h1>
+    <Form>
+      <Form.Group controlId="formUsername">
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="Username" placeholder="Username" />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+      <Form.Group controlId="formPassword">
+        <Form.Label>Confirm Password</Form.Label>
+        <Form.Control type="password" placeholder="Confirm Password" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Sign Up
+      </Button>
+    </Form>
+  </div>;
 }
