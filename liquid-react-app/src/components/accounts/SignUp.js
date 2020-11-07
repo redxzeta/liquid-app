8bef824b058f6b1357f9ce2f8e29b52b04308176
jordanhilado <37820188+jordanhilado@users.noreply.github.com>
2020-11-06 21:38:46 -0800

    Revert "Hooks update for login/signup"
    This reverts commit bcd23ae0f2c9241a1b689c120b23637468419fe1.

--- liquid-react-app/src/components/accounts/SignUp.js
+++ liquid-react-app/src/components/accounts/SignUp.js
@@ -1,31 +1,5 @@
 import React from "react";
 
-<<<<<<< Updated upstream
 export default function SignUp() {
   return <div>Signup</div>;
-=======
-export default function Login() {
-  return <div>
-    <h1 className="signup-box">Sign Up</h1>
-    <div className="signup-box">
-      <Form>
-        <Form.Group controlId="formUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="Username" placeholder="Username" />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Form.Group controlId="formPassword">
-          <Form.Label>Confirm Password</Form.Label>
-          <Form.Control type="password" placeholder="Confirm Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Sign Up
-        </Button>
-      </Form>
-    </div>
-  </div>;
->>>>>>> Stashed changes
 }
