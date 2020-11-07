41e5f3908f9af2c8c9db4859f276d897bc8e744b
redxzeta <xzetanzero@gmail.com>
2020-11-07 13:40:50 -0800

    quick fix

--- liquid-react-app/src/components/accounts/Login.js
+++ liquid-react-app/src/components/accounts/Login.js
@@ -1,23 +1,25 @@
-import React, { useState } from "react";
-import './login.css'; 
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import React from "react";
+import "./login.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function Login() {
-  return <div>
-    <h1>Login</h1>
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="Username" placeholder="Username" />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
-  </div>;
+  return (
+    <div>
+      <h1>Login</h1>
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="Username" placeholder="Username" />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
+  );
 }
--- liquid-react-app/src/components/accounts/SignUp.js
+++ liquid-react-app/src/components/accounts/SignUp.js
@@ -1,28 +1,29 @@
 import React from "react";
-import './login.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./login.css";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 export default function Login() {
-  return <div>
-    <h1>Sign Up</h1>
-    <Form>
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username</Form.Label>
-        <Form.Control type="Username" placeholder="Username" />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Confirm Password</Form.Label>
-        <Form.Control type="password" placeholder="Confirm Password" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Sign Up
-      </Button>
-    </Form>
-  </div>;
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <Form>
+        <Form.Group controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control type="Username" placeholder="Username" />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+        <Form.Group controlId="formPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control type="password" placeholder="Confirm Password" />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Sign Up
+        </Button>
+      </Form>
+    </div>
+  );
 }
