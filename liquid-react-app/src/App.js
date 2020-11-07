d5d4a228aacfe87b4f9f9cab5b682cfa67378d27
redxzeta <xzetanzero@gmail.com>
2020-11-06 19:41:50 -0800

    update links

--- liquid-react-app/src/App.js
+++ liquid-react-app/src/App.js
@@ -4,22 +4,24 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import LandingPage from "./components/home/LandingPage";
 import SignUp from "./components/accounts/SignUp";
 import Profile from "./components/profile/Profile";
+import { Container } from "react-bootstrap";
 export default function App() {
   return (
     <Router>
       <NavigationBar />
-      <h1>TRES</h1>
-      <Switch>
-        <Route path="profile">
-          <Profile />
-        </Route>
-        <Route path="/signup">
-          <SignUp />
-        </Route>
-        <Route path="/">
-          <LandingPage />
-        </Route>
-      </Switch>
+      <Container>
+        <Switch>
+          <Route path="profile">
+            <Profile />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </Container>
     </Router>
   );
 }
--- liquid-react-app/src/layout/NavigationBar.js
+++ liquid-react-app/src/layout/NavigationBar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Nav, NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom";
 export default function NavigationBar() {
   return (
     <Navbar bg="primary" expand="lg">
@@ -8,8 +9,15 @@ export default function NavigationBar() {
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
+          <Nav.Link as={Link} to="/">
+            Home
+          </Nav.Link>
+          <Nav.Link as={Link} to="/signup">
+            SignUp
+          </Nav.Link>
+          <Nav.Link as={Link} to="/login">
+            Login
+          </Nav.Link>
           <NavDropdown title="Dropdown" id="basic-nav-dropdown">
             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
             <NavDropdown.Item href="#action/3.2">
